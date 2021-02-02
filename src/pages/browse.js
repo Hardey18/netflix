f1e16150ec9d87636efb6a70d202f02714f79506
Adewale Nurudeen <nurudeenadeolaa@gmail.com>
2021-02-02 15:03:44 +0100

    all features done

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
 // import { BrowseContainer } from '../containers/browse';
-// import { useContent } from '../hooks';
-// import { selectionFilter } from '../utils';
+import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
 
 export default function Browse() {
-//   const { series } = useContent('series');
-//   const { films } = useContent('films');
-//   const slides = selectionFilter({ series, films });
+  const { series } = useContent('series');
+  // console.log("Series", series)
+  const { films } = useContent('films');
+  // console.log("Films", films)
+  const slides = selectionFilter({ series, films });
+  console.log("Slides", slides)
 
   return (
-    <div>Hello from Browse</div>
+    <BrowseContainer slides={slides} />
   )
 }
